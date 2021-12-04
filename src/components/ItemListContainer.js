325233436757f9d4f9d162deff044360e7666d24
Nel Rodriguez <nel.rodriguez22@gmail.com>
2021-12-03 21:58:52 -0300

    rodriguez-tienda-desafio4

--- src/components/ItemListContainer.js
+++ src/components/ItemListContainer.js
@@ -1,9 +1,13 @@
+import {ItemCount} from "./ItemCount"
 
 
-export const  ItemListContainer = (props) => {
+
+
+export const ItemListContainer = (props) => {
 	return (
 		<>
 			<h2 style={{color: "red"}}>Hola,{props.greeting} </h2>
+			<ItemCount stock={10} initial={1}/>
 		</>
 	)
 }
